fb13ee70e4762d91ce060ab429f01b30517efefa
Andrei <zaveransky.andrey@gmail.com>
2022-06-29 20:07:09 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// Data types: integers
+byte b;    // [0..255]
+sbyte sb;  // [-128..127]
+short sh;  // [-32768..32767]
+ushort us; // [0..65535]
+int i; // [-2^31..2^31-1]
+uint ui; // [0..2^32-1]
+long ln; // huge ;)
+ulong ul; // unsigned huge ;)
+
+// Data types: floating point
+float fl; // normal precision: 7 digits after floating point
+double db; // double precision: 15 digits after floating point
+
+// Data types: symbols and text
+char ch = 'a'; // only stores a single character
+string str = "Hello, World!"; // contains a string/line of text
+
+// Data types: logical
+bool bo = false; // either true or false
+
+int age = 15;
