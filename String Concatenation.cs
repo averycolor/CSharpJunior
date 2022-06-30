4ec1c3e2b9a41ce9c912124d7e845b0c51b3e38c
Andrei <zaveransky.andrey@gmail.com>
2022-06-30 21:42:45 +0300

    Update String Concatenation.cs

--- String Concatenation.cs
+++ String Concatenation.cs
@@ -1,4 +1,4 @@
-﻿string a = "Hello, ";
+string a = "Hello, ";
 string b = "World!";
 
 string result = a + b;
@@ -11,3 +11,10 @@ int ageNow = 13;
 string name = "Andrew";
 string text = "Congrats, " + name + ", you will be " + (ageNow + 1) + " tomorrow!";
 // If not for the parenthesis ageNow + 1 would be 131 and not 14 as desired.
+
+// String interpolation/formatting is also possible
+string c = "54";
+string d = "42";
+
+Console.WriteLine("I am {c} years old, and my friend is {d}");
+// This is much more straightforward and looks cleaner and readable as compared to using the + sign.
