21da04b85b1cb342e1eb815b72cb22ecfb5fdaa5
Andrei <zaveransky.andrey@gmail.com>
2022-06-30 08:43:20 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿bool equals, greaterThan, lesserThan;
+int a = 5;
+int b = 6;
+
+equals = a == b;
+greaterThan = a > b;
+lesserThan = a < b;
+
+Console.WriteLine(equals);
+Console.WriteLine(greaterThan);
+Console.WriteLine(lesserThan);
