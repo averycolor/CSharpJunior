a2a3b6fa8dcb13f1d8df8f0d9b78901fbef6cd48
Andrei <zaveransky.andrey@gmail.com>
2022-07-02 07:16:00 +0300

    Update AgeRestriction.cs

--- AgeRestriction.cs
+++ AgeRestriction.cs
@@ -1,9 +1,14 @@
 Console.Write("How old are you? ");
 int age = Convert.ToInt32(Console.ReadLine());
 
+Console.Write("What are your test scores?");
+int testScore = Convert.ToInt32(Console.ReadLine());
+int passingScore = 75;
+
 int passportAge = 14;
 int drivingAge = 16;
 int drinkingAge = 18;
+int universityAge = 17;
 
 string message = $"Since you are {age} years old, ";
 
@@ -21,6 +26,11 @@ else if (age >= passportAge)
 }
 else {
     message = "you cannot have a passport, drive, or drink.";
+}
+
+// && (and) and || (or) can be used to create complex boolean expressions consisting of multiple conditions
+if (testScore >= passingScore && age >= universityAge) {
+    message += " " + "You are also eligible for a scholarship at Yale.";
 }
 
 Console.WriteLine(message);
